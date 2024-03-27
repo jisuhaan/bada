c416311e54b28530a939e105d3022d318a27484e
han jisu <haanjisu@gmail.com>
2024-03-27 17:46:39 +0900

    한지수 - api 적용 중간 과정(24.03.27 17:46)

--- src/main/java/com/ezen/bada/weathers/WeatherController.java
+++ src/main/java/com/ezen/bada/weathers/WeatherController.java
@@ -3,6 +3,7 @@ package com.ezen.bada.weathers;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,11 +12,19 @@ public class WeatherController {
 	@Autowired
 	SqlSession sqlsession;
 	
-	
+
 	@RequestMapping(value = "/sea_info")
-	public String weather1() {
-		
+	public String weather1(Model model) {
+		Bada_list_DTO weatherData = fetchWeatherData();
+        // 가져온 날씨 정보를 모델에 추가하여 JSP로 전달
+        model.addAttribute("weatherData", weatherData);
 		return "sea_info";
+	}
+
+
+	private Bada_list_DTO fetchWeatherData() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }

9cfc75c2466c5abe1731548f44f5a81ce313fb71
jeong ji su <jisu301203@naver.com>
2024-04-03 17:16:23 +0900

    정지수 - 문의 출력 및 출력 형식 조정(24.04.03 17:16)

--- src/main/java/com/ezen/bada/inquire/Service.java
+++ src/main/java/com/ezen/bada/inquire/Service.java
@@ -1,6 +1,8 @@
 package com.ezen.bada.inquire;
 
 
+import java.util.ArrayList;
+
 import com.ezen.bada.member.MemberDTO;
 
 public interface Service {
@@ -9,6 +11,8 @@ public interface Service {
 
 	void inquire_save(String title, String category, String name, String id, String pic1, String pic2, String pic3,
 			String pic4, String pic5, String content, String secret, String secret_pw);
+
+	ArrayList<InquireDTO> inquire_listout();
 
 	
 

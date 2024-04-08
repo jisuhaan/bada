60bf1f35ea1e89d306bc86dbbac3429eb2dfd5aa
jeong ji su <jisu301203@naver.com>
2024-04-08 12:37:19 +0900

    정지수 - 문의 검색/추천/조회수 기능 완료, 신고하기 기능 구현 과정(24.04.08 12:37)

--- src/main/java/com/ezen/bada/inquire/InquireController.java
+++ src/main/java/com/ezen/bada/inquire/InquireController.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -221,71 +222,154 @@ public class InquireController {
     }
 	
 	
+	
 	@RequestMapping(value="/inquire_search")
     public String inquire_search(HttpServletRequest request, Model mo) {
 		
-		String search_keyword=request.getParameter("search_keyword");
-		String search_value=request.getParameter("search_value");
+		String keyword=request.getParameter("search_keyword");
+		String value=request.getParameter("search_value");
 		String category=request.getParameter("category");
 		String i_date=request.getParameter("i_date");
-		System.out.println("서치 키워드: "+search_keyword+"  서치값: "+search_value+"   카테고리: "+category+"   날짜: "+i_date);
-		
-		mo.addAttribute("search_keyword", search_keyword);
-	    mo.addAttribute("search_value", search_value);
-		
-		// 맨 처음 아웃 페이지를 들어갔을 때, 
-		// 사용자 화면에 떠야 할 1) 현재 페이지 위치와 2) 페이지 당 들어가는 레코드 수 설정 -> 눌 값일 때마다 적절한 초기화 해주기
-	    String nowPage=request.getParameter("nowPage");
-	    String cntPerPage=request.getParameter("cntPerPage");
-	  /* String 클래스는 객체이기 때문에 == null을 사용 가능하지만, int같은 기본 자료형은 불가하므로,
-	   * if문에서 null 확인을 위해 위에서는 String으로 받고, 인수로 넣기 직전에 intger로 변환해줘야 한다.
-	   */ 
-	    if(nowPage==null && cntPerPage == null) {
-	       nowPage="1";
-	       cntPerPage="5";
-	    }
-	    else if(nowPage==null) {        nowPage="1";
-	    }
-	    else if(cntPerPage==null) {
-	       cntPerPage="5";
-	    }      
-	    System.out.println("현재 페이지 : "+nowPage); // 어디에 있냐에 따라 다름
-	    System.out.println("페이지 당 레코드 수 : "+cntPerPage); // 5개
-	    
-	    // 3) 검색 결과 수 DB에서 구해오기
-	    Service ss = sqlsession.getMapper(Service.class);
-	    int total=ss.inquire_list_total_search(search_keyword, search_value, category, i_date);
-	    System.out.println("서치 결과 나온 레코드의 개수 : "+total);
-	     
-	    // 생성자로 나머지 페이지 처리에 필요한 필드값들도 모두 계산
-	    // 3가지 인수 넣어주기
-	    PageDTO dto=new PageDTO(total,Integer.parseInt(nowPage),Integer.parseInt(cntPerPage));
-	    int start=dto.getEnd();
-	    int end=dto.getStart();
-	    System.out.println("스타트: "+start+"  엔드: "+end);
-	    
-	    mo.addAttribute("paging",dto);
-	    // 리스트 안에 특정 페이지마다 출력될 5개 묶음의 레코드 모음 저장
-	    mo.addAttribute("list",ss.page_inquire_listout_search(search_keyword, search_value, category, i_date, start, end));
-	    
-	    System.out.println("내용물: "+ss.page_inquire_listout_search(search_keyword, search_value, category, i_date, start, end));
+		System.out.println("서치 키워드: "+keyword+"  서치값: "+value+"   카테고리: "+category+"   날짜: "+i_date);
+		Service ss = sqlsession.getMapper(Service.class);
+		ArrayList<InquireDTO> list;
+	      
+	         if(category.equals("") && i_date.equals("")) { //카테고리와 날짜를 모두 입력하지 않은 경우
+	            list=ss.inquire_search_out1(keyword, value);
+	         }//내부 if문 끝
+	         else if(category.equals("")) { //카테고리는 입력하지 않고 날짜는 입력한 경우
+	            list=ss.inquire_search_out2(keyword, value, i_date);
+	         }//내부 else if문 끝
+	         else if(i_date.equals("")) { //카테고리는 입력하고 날짜는 입력하지 않은 경우
+	            list=ss.inquire_search_out3(keyword, value, category);
+	         }//내부 else if문2 끝
+	         else { //카테고리와 날짜를 모두 입력한 경우
+	            list=ss.inquire_search_out4(keyword, value, category, i_date);
+	         }//내부 else문 끝
+	      
+	      System.out.println(list);
+	      
+	    mo.addAttribute("list", list);
 	    
 	    return "inquire_search_view";
     }
 	
 	
-	
-	
-	@RequestMapping(value = "/inquire_detail")
-	public String inquire_detail(HttpServletRequest request,Model mo) {
-		String inquire_num=request.getParameter("inquire_num");
+	@RequestMapping(value = "/inquire_secret_yn")
+	public String inquire_secret_yn(HttpServletRequest request, Model mo) {
+		int inquire_num=Integer.parseInt(request.getParameter("inquire_num"));
+		String secret=request.getParameter("secret");
 		
 		Service ss=sqlsession.getMapper(Service.class);
 		InquireDTO dto=ss.inquire_detail(inquire_num);
 		mo.addAttribute("dto", dto);
 		
+		if(secret.equals("y")) {return "inquire_secret_yn";}
+		else {return "redirect:/to_inquire_detail?inquire_num=" + inquire_num;}
+		
+	}
+	
+	
+	@RequestMapping(value = "/to_inquire_detail")
+	public String inquire_detail(HttpServletRequest request,Model mo) {
+		int inquire_num=Integer.parseInt(request.getParameter("inquire_num"));
+		
+		Service ss=sqlsession.getMapper(Service.class);
+		ss.inquire_updatecnt(inquire_num);
+		InquireDTO dto=ss.inquire_detail(inquire_num);
+		mo.addAttribute("dto", dto);
+		
 		return "inquire_detail";
 	}
+	
+	
+	@RequestMapping(value = "/inquire_recommand")
+	public String inquire_recommand(HttpServletRequest request, Model mo, HttpServletResponse response) throws IOException, ServletException {
+		int inquire_num=Integer.parseInt(request.getParameter("inquire_num"));
+		String loginid=request.getParameter("loginid");
+		
+		Service ss=sqlsession.getMapper(Service.class);
+		int rec_id_co=ss.inquire_rec_id(inquire_num, loginid);
+		
+		if(rec_id_co==0) {
+		ss.inquire_recommand(loginid, inquire_num);
+		InquireDTO dto=ss.inquire_detail(inquire_num);
+		mo.addAttribute("dto", dto);
+		}
+		
+		else {
+	        InquireDTO dto=ss.inquire_detail(inquire_num);
+			mo.addAttribute("dto", dto);
+		}
+		
+		return "inquire_detail";
+		}
+		
+	
+	@RequestMapping(value = "/inquire_report_view")
+	public String inquire_report_view(HttpServletRequest request,Model mo) {
+		int inquire_num=Integer.parseInt(request.getParameter("inquire_num"));
+		String loginid=request.getParameter("loginid");
+		
+		Service ss=sqlsession.getMapper(Service.class);
+		
+		MemberDTO mdto = ss.input_info(loginid);
+		mo.addAttribute("mdto", mdto);
+		
+		InquireDTO idto=ss.inquire_detail(inquire_num);
+		mo.addAttribute("idto", idto);
+		
+		return "inquire_report_view";
+	}
+	
+	  @RequestMapping(value = "/inquire_ban_check")
+	   public String inquire_ban_check(HttpServletRequest request) throws IOException {
+	      
+	        String id = request.getParameter("id");
+	        int ban_inquire_num=Integer.parseInt(request.getParameter("ban_inquire_num"));
+	        String category = request.getParameter("category");
+	        String content = request.getParameter("content");
+	        
+	        System.out.println("체크 1: "+id);
+	        System.out.println("체크 2: "+ban_inquire_num);
+	        System.out.println("체크 3: "+category);
+	        System.out.println("체크 4: "+content);
+	
+	        Service ss=sqlsession.getMapper(Service.class);
+	        
+	        int inquire_check=ss.inquire_ban_check(id, ban_inquire_num, category, content); //동일한 사람이 동일한 글을 동일한 사유로 여러번 신고할 수 없도록 중복 방지
+	        String result="";
+	        if (inquire_check==0) {
+	        	result="ok";
+	        }
+	        else {result="nope";}
+	    
+	      return result;
+	   }
+	  
+	  @RequestMapping(value = "/inquire_ban_save", method = RequestMethod.POST)
+	   public String inquire_ban_save(HttpServletRequest request, Model mo) throws IOException {
+	      
+	      	String title = request.getParameter("title");
+	        String name = request.getParameter("name");
+	        String id = request.getParameter("id");
+	        int ban_inquire_num=Integer.parseInt(request.getParameter("ban_inquire_num"));
+	        String ban_name = request.getParameter("bqn_name");
+	        String ban_id = request.getParameter("ban_id");
+	        String category = request.getParameter("category");
+	        String content = request.getParameter("content");
+	
+	        Service ss=sqlsession.getMapper(Service.class);
+	        ss.inquire_ban_save(title, name, id, ban_inquire_num, ban_name, ban_id, category, content);
+	        
+	        InquireDTO dto=ss.inquire_detail(ban_inquire_num);
+			mo.addAttribute("dto", dto);
+	    
+	      return "inquire_detail";
+	   }
+	   
+	
+	
 	
 	
 	
